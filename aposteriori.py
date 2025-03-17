53275288f359f88a2bbfb794cffbe7d3753270c6
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-03-17 10:57:20 +0200

    split properly by comment

--- aposteriori.py
+++ aposteriori.py
@@ -6,26 +6,32 @@ import scipy
 from ndfu import ndfu
 
 
-def aposteriori_unimodality(all_annotations: list[int], annotator_group: list[Any], comment_group: list[Any]) -> float:
-    all_annotations = np.array(all_annotations)
+def aposteriori_unimodality(
+    annotations: list[int], annotator_group: list[Any], comment_group: list[Any]
+) -> float:
+    _validate_input(annotations, annotator_group, comment_group)
+    all_annotations = np.array(annotations)
     annotator_group = np.array(annotator_group)
+    comment_group = np.array(comment_group)
 
     all_comment_stats = []
     for curr_comment_id in np.unique(comment_group):
         is_in_curr_comment = comment_group == curr_comment_id
         all_comment_annotations = all_annotations[is_in_curr_comment]
         comment_annotator_groups = annotator_group[is_in_curr_comment]
-        
-        curr_comment_stats = _aposteriori_comment(all_comment_annotations, comment_annotator_groups)
-        print(curr_comment_stats)
-        all_comment_stats.append(curr_comment_stats)
 
+        curr_comment_stats = _aposteriori_comment(
+            all_comment_annotations, comment_annotator_groups
+        )
+        all_comment_stats.append(curr_comment_stats)
 
     print("DEBUG: final stats: ", all_comment_stats)
     return _significance(all_comment_stats)
 
 
-def _aposteriori_comment(all_comment_annotations: np.ndarray, annotator_group: np.ndarray) -> float:
+def _aposteriori_comment(
+    all_comment_annotations: np.ndarray, annotator_group: np.ndarray
+) -> float:
     """
     Generate the aposteriori stat (ndfu diff stat) for each annotation level, for one comment.
 
@@ -41,12 +47,9 @@ def _aposteriori_comment(all_comment_annotations: np.ndarray, annotator_group: n
         factor_annotations = all_comment_annotations[annotator_group == annot_group]
 
         if len(factor_annotations) != 0:
-            aposteriori_stat = _ndfu_diff(
-                all_comment_annotations, factor_annotations
-            )
+            aposteriori_stat = _ndfu_diff(all_comment_annotations, factor_annotations)
             stats.append(aposteriori_stat)
     return np.max(stats)
-
 
 
 def _significance(level_aposteriori_statistics: list[float]) -> float:
@@ -62,12 +65,10 @@ def _significance(level_aposteriori_statistics: list[float]) -> float:
     """
     x = level_aposteriori_statistics
     y = np.zeros_like(level_aposteriori_statistics)
-    return scipy.stats.mannwhitneyu(x, y, alternative="less").pvalue
+    return scipy.stats.mannwhitneyu(x, y, alternative="greater").pvalue
 
 
-def _ndfu_diff(
-    global_annotations: np.ndarray, level_annotations: np.ndarray
-) -> float:
+def _ndfu_diff(global_annotations: np.ndarray, level_annotations: np.ndarray) -> float:
     """
     Computes the difference in normalized distance from unimodality (nDFU) between
     global annotations and annotations from a specific group.
@@ -83,3 +84,15 @@ def _ndfu_diff(
     global_ndfu = ndfu(global_annotations)
     level_ndfu = ndfu(level_annotations)
     return global_ndfu - level_ndfu
+
+
+def _validate_input(
+    annotations: list[int], annotator_group: list[Any], comment_group: list[Any]
+) -> None:
+    if not (len(annotations) == len(annotator_group) == len(comment_group)):
+        raise ValueError(
+            "Length of provided lists must be the same, " +
+            f"but len(annotations)=={len(annotations)}, " +
+            f"len(annotator_group)=={len(annotator_group)}, " +
+            f"len(comment_group)=={len(comment_group)}"
+        )
