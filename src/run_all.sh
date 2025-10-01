fd6e832f5be93cbb683193ca06153c8f3d0965d4
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2025-10-01 14:21:46 +0300

    run experiments using GNU parallel
    avoids rogue processes in case of CTRL C

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,19 +1,8 @@
-python -m src.real_life_sap \
-    --dataset-path=data/sap.csv \
-    --latex-output-dir=manuscript/generated &
+#!/bin/bash
 
-python -m src.synthetic_100 \
-    --dataset-path=data/100_annotators.csv \
-    --latex-output-dir=manuscript/generated &
-
-python -m src.synthetic_vmd \
-    --dataset-path=data/vmd.csv \
-    --latex-output-dir=manuscript/generated &
-
-python -m src.real_life_kumar \
-    --dataset-path=data/kumar.json \
-    --latex-output-dir=manuscript/generated &
-
-wait
-
-echo "Finished all experiments :)"
+parallel --jobs 4 --delay 0.1 \
+    ::: \
+    'python -m src.real_life_sap --dataset-path=data/sap.csv --latex-output-dir=manuscript/generated' \
+    'python -m src.synthetic_100 --dataset-path=data/100_annotators.csv --latex-output-dir=manuscript/generated' \
+    'python -m src.synthetic_vmd --dataset-path=data/vmd.csv --latex-output-dir=manuscript/generated' \
+    'python -m src.real_life_kumar --dataset-path=data/kumar.json --latex-output-dir=manuscript/generated'
